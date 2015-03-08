593b882f55fcb3fc4ca7b88a80bf9e71bb477cff
Huang Ying-Kai <huangying-kai@Huangs-MacBook-Pro.local>
2015-03-08 13:59:16 +0800

    Initial Commit

---
+++ FinalProject2/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  FinalProject2
+//
+//  Created by Huang Ying-Kai on 2015/3/8.
+//  Copyright (c) 2015年 Huang Ying-Kai. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
